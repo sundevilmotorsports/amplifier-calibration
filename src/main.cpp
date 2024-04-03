1af226242b3ea584f8048ea831ae238fcd8b498d
evanlerr <evan.w.butler03@gmail.com>
2024-04-03 13:26:31 -0700

    Updated Amplifier Code
    Using teensy 4.0 devboard instead

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@ FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> Can0;
 
 void canSniff(const CAN_message_t &msg);
 
+int counter;
+double sum;
+double avg;
+double avgF;
+
 void setup() {
   // put your setup code here, to run once:
   Can0.begin();
@@ -29,17 +34,25 @@ void loop() {
 void canSniff(const CAN_message_t &msg) {
   int rawAmplifierOutput = 0;
   int ambientTemp = 0;
-
+  int rolAvg = 0;
+  int prevValue = 0;
+  double force = 0;
+  
 
   switch(msg.id) {
     case 0x4E2: // TODO update ids
       Serial.print("id: ");
       Serial.print(msg.id, HEX);
       rawAmplifierOutput = (msg.buf[0] << 8) | msg.buf[1];
+      counter++;
+      sum += rawAmplifierOutput;
+      avg = sum / counter;
       ambientTemp = (msg.buf[4] << 8) | msg.buf[5]; // attempting to read ambient temp
-      Serial.print("  ");
-      Serial.println(rawAmplifierOutput);
-      break;
+      force = (4.7/10)*(rawAmplifierOutput-1432);
+      Serial.println("  " + String(avg) + ", " + String(force));
+      // rolAvg = (prevValue * .9 + rawAmplifierOutput*.1 )*10;
+
+      return;
     default:
     //Serial.print("No message");
     break;
